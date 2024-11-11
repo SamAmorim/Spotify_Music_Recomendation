8d46b0a0af6301cbc58df2926372a23241eaff16
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2024-11-11 02:54:13 -0300

    Criando gráficos

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,27 +1,252 @@
-const chartDiv = document.getElementById('chart');
-const genresChart = document.getElementById('genres-chart')
+const characteristicsChart = document.getElementById('characteristics-chart');
+const musicalityChart = document.getElementById('musicality-chart');
+const genresChart = document.getElementById('genres-chart');
+
+Chart.register(ChartDataLabels);
 
 (async function () {
     new Chart(
-        chartDiv,
+        characteristicsChart,
         {
-            type: 'line',
+            type: 'bar',
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        ticks: {
+                            color: "#fff"
+                        },
+                        grid: {
+                            display: false
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            color: "#fff",
+                            callback: value => value + "%"
+                        },
+                        grid: {
+                            color: "rgba(255, 255, 255, 0.25)"
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        labels: {
+                            color: '#fff',
+                            font: {
+                                size: 12
+                            }
+                        }
+                    },
+                    title: {
+                        display: false
+                    },
+                    datalabels: {
+                        display: false
+                    }
+                }
+            },
             data: {
-                labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+                labels: ['Acústica', 'Dançante', 'Energética', 'Positiva', 'Triste'],
                 datasets: [
                     {
-                        label: 'Músicas ouvidas',
-                        data: [10, 20, 30, 40, 50, 60],
-                        borderColor: 'rgb(255, 99, 132)',
-                        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                        label: 'Média',
+                        data: [10, 80, 30, 40, 60],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            }
+        }
+    );
+})();
+
+(async function () {
+    new Chart(
+        musicalityChart,
+        {
+            type: 'line',
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        ticks: {
+                            color: "#fff"
+                        },
+                        grid: {
+                            display: false
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            color: "#fff",
+                            callback: value => value + "%"
+                        },
+                        grid: {
+                            color: "rgba(255, 255, 255, 0.25)"
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        labels: {
+                            color: '#fff',
+                            font: {
+                                size: 12
+                            }
+                        }
+                    },
+                    title: {
+                        display: false
+                    },
+                    datalabels: {
+                        display: false
+                    }
+                }
+            },
+            data: {
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+                datasets: [
+                    {
+                        label: 'Acústica',
+                        data: [10, 30, 10, 80, 20, 30, 100],
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        cubicInterpolationMode: 'monotone',
+                        borderWidth: 1
                     },
                     {
-                        label: 'Minutos ouvidos',
-                        data: [20, 40, 60, 80, 100, 120],
-                        borderColor: 'rgb(54, 162, 235)',
-                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                        label: 'Dançante',
+                        data: [50, 20, 40, 60, 70, 80, 90],
+                        fill: false,
+                        borderColor: 'rgb(255, 99, 132)',
+                        cubicInterpolationMode: 'monotone',
+                        borderWidth: 1
                     },
+                    {
+                        label: 'Energética',
+                        data: [30, 40, 60, 70, 80, 90, 100],
+                        fill: false,
+                        borderColor: 'rgb(54, 162, 235)',
+                        cubicInterpolationMode: 'monotone',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Positiva',
+                        data: [40, 60, 80, 100, 20, 10, 50],
+                        fill: false,
+                        borderColor: 'rgb(255, 206, 86)',
+                        cubicInterpolationMode: 'monotone',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Triste',
+                        data: [70, 80, 90, 100, 50, 10, 80],
+                        fill: false,
+                        borderColor: 'rgb(153, 102, 255)',
+                        cubicInterpolationMode: 'monotone',
+                        borderWidth: 1
+                    }
                 ],
+                borderWidth: 1
+            }
+        }
+    );
+})();
+
+(async function () {
+    new Chart(
+        genresChart,
+        {
+            type: 'pie',
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 70,
+                        right: 70,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: false
+                    },
+                    datalabels: {
+                        // Put labels outside the chart
+                        color: '#fff',
+                        formatter: (value, context) => {
+                            const datapoints = context.chart.data.datasets[0].data;
+                            const total = datapoints.reduce((total, datapoint) => total + datapoint, 0);
+                            const percentage = value / total * 100;
+                            return context.chart.data.labels[context.dataIndex] + "\n" + percentage.toFixed(1) + "%";
+                        },
+                        font: {
+                            size: 12
+                        },
+                        textAlign: 'center',
+                        align: 'end',
+                        anchor: 'end',
+                        offset: 10
+                    }
+                }
+            },
+            data: {
+                labels: ['Electronic', 'Metal', 'Country', 'Classical', 'Jazz', 'Blues', 'Reggae', 'Folk', 'Punk', 'Soul'],
+                datasets: [{
+                    label: 'Músicas',
+                    data: [70, 80, 90, 100, 110, 120, 130, 140, 150, 160],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
             },
         }
     )
--- static/js/report.js
+++ static/js/report.js
@@ -1,66 +1,73 @@
 const reportTypeForm = document.getElementById('report-type-form');
 const recentlyPlayedContainer = document.getElementById('recently-played-container');
+const recentlyPlayedSpinner = document.getElementById('recently-played-spinner');
 const aiRecommendationContainer = document.getElementById('ai-recommendation-container');
+const aiRecommendationSpinner = document.getElementById('ai-recommendation-spinner');
 const trackComponent = document.getElementById('track-component');
 
-if (localStorage.getItem('recentlyPlayed')) {
-    const recentlyPlayed = JSON.parse(localStorage.getItem('recentlyPlayed'));
-    recentlyPlayed.forEach(item => {
-        const newTrackComponent = trackComponent.cloneNode(true);
-        newTrackComponent.style.display = 'block';
-        newTrackComponent.querySelector('#track-image').src = item.track.album.images[2].url;
-        newTrackComponent.querySelector('#track-image').alt = item.track.name;
-        newTrackComponent.querySelector('#track-name').textContent = item.track.name;
-        newTrackComponent.querySelector('#track-artist').textContent = item.track.artists[0].name;
-        recentlyPlayedContainer.appendChild(newTrackComponent);
-    });
-} else {
-    fetch('/spotify/recently-played')
+(async function getRecentlyPlayed() {
+    recentlyPlayedSpinner.style.display = 'block';
+
+    await fetch('/spotify/recently-played')
         .then(response => response.json())
-        .then(data => {
-            localStorage.setItem('recentlyPlayed', JSON.stringify(data.items));
+        .then(
+            data => {
+                if (data.error) {
+                    console.error(data);
+                    recentlyPlayedContainer.innerHTML = `<p class="text-center text-white">Erro ao carregar músicas recentes</p><p class="text-center text-white">${JSON.stringify(data.error)}</p>`;
+                    return;
+                }
 
-            data.items.forEach(item => {
-                const newTrackComponent = trackComponent.cloneNode(true);
-                newTrackComponent.style.display = 'block';
-                newTrackComponent.querySelector('#track-image').src = item.track.album.images[2].url;
-                newTrackComponent.querySelector('#track-image').alt = item.track.name;
-                newTrackComponent.querySelector('#track-name').textContent = item.track.name;
-                newTrackComponent.querySelector('#track-artist').textContent = item.track.artists[0].name;
-                recentlyPlayedContainer.appendChild(newTrackComponent);
-            });
-        });
-}
+                data.items.forEach(item => {
+                    const newTrackComponent = trackComponent.cloneNode(true);
+                    newTrackComponent.style.display = 'block';
+                    newTrackComponent.querySelector('#track-image').src = item.track.album.images[2].url;
+                    newTrackComponent.querySelector('#track-image').alt = item.track.name;
+                    newTrackComponent.querySelector('#track-name').textContent = item.track.name;
+                    newTrackComponent.querySelector('#track-artist').textContent = item.track.artists[0].name;
+                    recentlyPlayedContainer.appendChild(newTrackComponent);
+                });
+            },
+            error => {
+                console.error(error);
+                recentlyPlayedContainer.innerHTML = `<p class="text-center text-white">Erro ao carregar músicas recentes</p><p class="text-center text-white">${JSON.stringify(error)}</p>`;
+            }
+        );
 
-if (localStorage.getItem('aiRecommendation')) {
-    const aiRecommendation = JSON.parse(localStorage.getItem('aiRecommendation'));
-    aiRecommendation.forEach(item => {
-        const newTrackComponent = trackComponent.cloneNode(true);
-        newTrackComponent.style.display = 'block';
-        newTrackComponent.querySelector('#prediction-precision').textContent = item.similarity.toFixed(4);
-        newTrackComponent.querySelector('#track-image').src = item['track_data'].album.images[2].url;
-        newTrackComponent.querySelector('#track-image').alt = item['track_data'].name;
-        newTrackComponent.querySelector('#track-name').textContent = item['track_data'].name;
-        newTrackComponent.querySelector('#track-artist').textContent = item['track_data'].artists[0].name;
-        aiRecommendationContainer.appendChild(newTrackComponent);
-    });
-} else {
-fetch('/prediction')
-    .then(response => response.json())
-    .then(data => {
-        localStorage.setItem('aiRecommendation', JSON.stringify(data));
-        data.forEach(item => {
-            const newTrackComponent = trackComponent.cloneNode(true);
-            newTrackComponent.style.display = 'block';
-            newTrackComponent.querySelector('#prediction-precision').textContent = item.similarity.toFixed(4);
-            newTrackComponent.querySelector('#track-image').src = item['track_data'].album.images[2].url;
-            newTrackComponent.querySelector('#track-image').alt = item['track_data'].name;
-            newTrackComponent.querySelector('#track-name').textContent = item['track_data'].name;
-            newTrackComponent.querySelector('#track-artist').textContent = item['track_data'].artists[0].name;
-            aiRecommendationContainer.appendChild(newTrackComponent);
-        });
-    });
-}
+    recentlyPlayedSpinner.style.display = 'none';
+})();
+
+(async function getAiRecommendation() {
+    aiRecommendationSpinner.style.display = 'block';
+
+    fetch('/prediction')
+        .then(response => response.json())
+        .then(
+            data => {
+                if (data.error) {
+                    console.error(data);
+                    aiRecommendationContainer.innerHTML = `<p class="text-center text-white">Erro ao carregar recomendação</p><p class="text-center text-white">${JSON.stringify(data.error)}</p>`;
+                    return;
+                }
+
+                data.forEach(item => {
+                    const newTrackComponent = trackComponent.cloneNode(true);
+                    newTrackComponent.style.display = 'block';
+                    newTrackComponent.querySelector('#prediction-precision').textContent = item.similarity.toFixed(0) + "%";
+                    newTrackComponent.querySelector('#track-image').src = item['track_data'].album.images[2].url;
+                    newTrackComponent.querySelector('#track-image').alt = item['track_data'].name;
+                    newTrackComponent.querySelector('#track-name').textContent = item['track_data'].name;
+                    newTrackComponent.querySelector('#track-artist').textContent = item['track_data'].artists[0].name;
+                    newTrackComponent.querySelector('#audio-preview').src = item['track_data'].preview_url;
+                    aiRecommendationContainer.appendChild(newTrackComponent);
+                });
+            },
+            error => {
+                console.error(error);
+                aiRecommendationContainer.innerHTML = `<p class="text-center text-white">Erro ao carregar recomendação</p><p class="text-center text-white">${JSON.stringify(error)}</p>`;
+            }
+        );
+})();
 
 reportTypeForm.addEventListener('change', e => {
     if (e.target.id === 'recently-played') {
